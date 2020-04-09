093680ca36b4c42b343d58849eeb4e5b0b3019cc
Mitchell Budden <mitchbudden79@gmail.com>
2020-04-09 15:13:55 -0400

    Border color

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -9,9 +9,9 @@ class Contact extends Component {
       <div>
         <Nav selected="Contact" />
         <h1 className="last-item">
-          The Woodlake Construction team is here to help!<br></br> Please use
-          one of the methods below to get in contact and we will get back to you
-          as soon as possible.
+          <b>The Woodlake Construction team is here to help!</b>
+          <br></br> Please use one of the methods below to get in contact and we
+          will get back to you as soon as possible.
         </h1>
         <Footer />
       </div>
